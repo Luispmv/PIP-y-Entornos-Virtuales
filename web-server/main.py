7a5d5b847196f23b1d6d2041c1b3a80b39e1c671
luismujica024085 <luismujica024085@gmail>
2024-07-19 23:57:07 -0600

    Dockerizamos la carpeta Graficos y la carpeta Web-server

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,25 @@
 import store
 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+mi_nombre = "Pablo"
+
+@app.get("/")
+def get_list():
+	return [1,2,3]
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_list():
+	return f'<h1 style="color:#ff0000;">Hola Mundo mi nombre es {mi_nombre}</h1>'
+
 def run():
     store.get_categories()
 
 if __name__=="__main__":
     run()
+
+
+
